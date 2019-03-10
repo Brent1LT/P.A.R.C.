4f5923c9915fb0e379914b8c4ef62d9733896cdf
Gabriel F. Lujan <gfl.devo@gmail.com>
2019-03-09 20:39:50 -0800

    Create initial infra for frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>P.A.R.C.</h1>
+        <footer>
+          Copyright &copy; 2019 B.V.G.S.
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
