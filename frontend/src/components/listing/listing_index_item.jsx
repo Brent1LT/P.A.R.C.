c9637da7034150eb405c5a2453913e2521137f89
Vishavpreet Sandhu <vishal1949@gmail.com>
2019-03-11 13:49:18 -0700

    Add listing show container and add price and description to listing index item

--- frontend/src/components/listing/listing_index_item.jsx
+++ frontend/src/components/listing/listing_index_item.jsx
@@ -5,7 +5,6 @@ class ListingIndexItem extends React.Component{
         super(props)
     }
     
-
     render(){
         return(
             <div>
@@ -13,6 +12,8 @@ class ListingIndexItem extends React.Component{
                     <img src="https://images.vexels.com/media/users/3/128399/isolated/lists/4c21120637e7ad87ca7d800c3d24eb21-parking-round-service-icon.png" alt=""/>
                     <div>{this.props.listing.address.street}</div>
                     <div>{this.props.listing.address.city}*{this.props.listing.address.staet}</div>
+                    <div>{this.props.listing.address.description}</div>
+                    <div>{this.props.listing.address.description}</div>
                 </div>
             </div>
         )
