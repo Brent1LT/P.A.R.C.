ae2acae837288e6d3df6f408fa571ada9cc8ca28
Sam Ardrey <samardrey@gmail.com>
2019-03-11 14:10:23 -0700

    Progress

--- routes/api/bookings.js
+++ routes/api/bookings.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const Booking = require('../../models/Booking');
 const validateBookingsInput = require('../../validations/bookings');
 
-router.post('/',
+router.post('/:listingId',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const { errors, isValid } = validateBookingsInput(req.body);
@@ -12,9 +12,14 @@ router.post('/',
     if (!isValid) {
       return res.status(400).json(errors);
     }
+    let startDate = new Date(req.body.startDate)
+    let endDate = new Date(req.body.endDate);
 
     const newBooking = new Booking ({
-      text: req.body.text,
+      startDate,
+      endDate,
+      offMarket: req.body.offMarket,
+      listing: req.params.listingId,
       user: req.user.id
     });
 
@@ -22,15 +27,23 @@ router.post('/',
   }
 );
 
-router.get(`/:bookingId`,
+router.get(`/:userId`,
   (req, res) => {
-    const bookings = Booking.find({ listing: req.params.id })
+    Booking.find({ user: req.params.userId })
     .then(bookings => res.json(bookings))
     .catch(err =>
       res.status(404).json({ nobookingsfound: "No bookings found for that listing"}
       )
     );
   }
+);
+
+router.delete(`/delete/:id`,
+  Booking.findByIdAndRemove({_id: req.params.id},
+    function(err, item){
+      if(err) res.json(err);
+      else res.json('Successfully removed');
+  })
 );
 
 module.exports = router;
