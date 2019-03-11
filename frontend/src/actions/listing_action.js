d63ee66177a4e25d9922477358b15243910d65be
Vishavpreet Sandhu <vishal1949@gmail.com>
2019-03-11 10:56:27 -0700

    Add listing_actions and create entities reducer

--- frontend/src/actions/listing_action.js
+++ frontend/src/actions/listing_action.js
@@ -3,6 +3,8 @@ import * as ListingApiUtil from '../util/listing_api_util'
 export const RECEIVE_LISTING = 'RECEIVE_LISTING';
 export const RECEIVE_ALL_LISTINGS = 'RECEIVE_ALL_LISTINGS';
 export const DELETE_LISTING = 'DELETE_LISTING'; 
+export const RECEIVE_LISTING_ERRORS = 'RECEIVE_LISTING_ERRORS';
+export const CLEAR_LISTING_ERRORS = 'CLEAR_ERRORS';
 
 
 const receiveListing = listing => {
@@ -26,36 +28,67 @@ const deleteListing = (listing) => {
     })
 }
 
+const receiveListingErrors = errors => {
+    return({
+        type: RECEIVE_LISTING_ERRORS,
+        errors
+    })
+}
+
+export const clearListingErrors = () => {
+    return({
+        type: CLEAR_LISTING_ERRORS
+    })
+} 
+
 
 //thunk action creators
 
 export const createListing = listing => dispatch => {
     //listing above             ^      is the listing the user creates
-    ListingApiUtil.createListing(listing).then(listing => {
-        // same listing just sending to backend
-        dispatch(receiveListing(listing))
+    ListingApiUtil.createListing(listing).then(
+        listing => {
+            // same listing just sending to backend
+            dispatch(receiveListing(listing)),
+        errors => {
+            dispatch(receiveListingErrors(errors))
+        }
     })
 }
 
 export const fetchListing = id => dispatch => {
     //supply an id for the axios call to check for
-    ListingApiUtil.fetchListing(id).then(listing => {
-        // listing is what is supplied by the backend 
-        dispatch(receiveListing(listing))
+    ListingApiUtil.fetchListing(id).then(
+        listing => {
+            // listing is what is supplied by the backend 
+            dispatch(receiveListing(listing)),
+        errors => {
+            dispatch(receiveListingErrors(errors))
+        }
     })
 }
 
 export const fetchListings = () => dispatch => {
-    ListingApiUtil.fetchAllListings().then(listings => {
-        //listings is what our backend is returning
-        dispatch(receiveAllListings(listings))
+    ListingApiUtil.fetchAllListings().then(
+        listings => {
+            //listings is what our backend is returning
+            dispatch(receiveAllListings(listings)),
+        errors => {
+            dispatch(receiveListingErrors(errors))
+        }
     })
 }
 
 export const deleteListing = (id) => dispatch => {
-    ListingApiUtil.deleteListing(id).then( listing => {
-        //backend will delete the listing
-        dispatch(deleteListing(listing))
+    ListingApiUtil.deleteListing(id).then( 
+        listing => {
+            //backend will delete the listing
+            dispatch(deleteListing(listing)),
+        errors => {
+            dispatch(receiveListingErrors(errors))
+        }
     })
 }
+
+
 
