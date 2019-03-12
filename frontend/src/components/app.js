856a5aed1180fb67d1ebf7c97e35a9611b07b897
Gabriel F. Lujan <gfl.devo@gmail.com>
2019-03-11 21:50:02 -0700

    Add booking form and google map to listing show page, update route for listing show page in app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 // import NavBarContainer from './nav/navbar_container';
+import ListingShowContainer from './listing/listing_show_container';
 
 import MainPage from './main/main_page';
 
@@ -9,6 +10,8 @@ const App = () => (
   <Switch>
     <AuthRoute exact path="/" component={MainPage} />
     {/* <ProtectedRoute exact path='/listings' component={ListingsPage} /> */}
+    {/* <ProtectedRoute exact path="/listings/:listingId" component={ListingShowContainer} /> */}
+    <Route exact path="/listings/:listingId" component={ListingShowContainer} />
     {/* <ProtectedRoute exact path='/bookings' component={BookingsPage} /> */}
     {/* add components/routes here for login/signup modal? */}
   </Switch>
--- frontend/src/components/listing/listing_show.jsx
+++ frontend/src/components/listing/listing_show.jsx
@@ -1,34 +1,35 @@
-import React from 'react' 
+import React from 'react'
+import GoogleMapContainer from '../map/map';
+import BookingFormContainer from '../bookings/booking_form_container';
 
 class ListingShow extends React.Component{
-    constructor(props){
-        super(props)
+  constructor(props){
+    super(props)
+  }
 
+  componentDidMount(){
+    this.props.fetchListing();
+  }
+
+  render(){
+    if(this.props.listing === undefined){
+      return null;
     }
-
-    componentDidMount(){
-        this.props.fetchListing();
-    }
-
-    render(){
-        if(this.props.listing === undefined){
-            return null;
-        }
-        return(
-            <div>
-                <div>MAP GOES HERE</div>
-                <div>
-                    <h2>{this.props.listing.address.street} {this.props.listing.address.city}</h2>
-                    <div>Description
-                        <div>{this.props.listing.description}</div>
-                    </div>
-                    <div>BOOKING FORM</div>
-                </div>
-                <div>IMAGE BELOW THIS</div>
-                <img src="" alt=""/>
+    return(
+      <div>
+          <GoogleMapContainer />
+          <div>
+            <h2>{this.props.listing.address.street} {this.props.listing.address.city}</h2>
+            <div>Description
+              <div>{this.props.listing.description}</div>
             </div>
-        )
-    }
+            <BookingFormContainer />
+          </div>
+          <div>IMAGE BELOW THIS</div>
+          <img src="" alt=""/>
+      </div>
+    )
+  }
 }
 
-export default ListingShow
+export default ListingShow;
