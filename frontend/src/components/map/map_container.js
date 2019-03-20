267e2d6cf3b57739208460a96cf28554dc305770
Sam Ardrey <samardrey@gmail.com>
2019-03-20 16:46:04 -0700

    yar

--- frontend/src/components/map/map_container.js
+++ frontend/src/components/map/map_container.js
@@ -1,29 +1,18 @@
 import { connect } from 'react-redux';
 import GoogleMapFinal from './map';
-<<<<<<< HEAD
-
-const mapStateToProps = (state, ownProps) => {
-  return ({
-    listings: ownProps.listings,
-=======
 import { fetchListings } from '../../actions/listing_action';
 
 const mapStateToProps = (state, ownProps) => {
   let listings = Object.values(state.entities.listings);
   return ({
     listings,
->>>>>>> 1f0dbbb0e76963af6f1e0b73fb29429cd75b322c
     style: ownProps.style,
   });
 };
 const mapDispatchToProps = (dispatch) => {
-<<<<<<< HEAD
-  return ({});
-=======
   return ({
     fetchAllListings: () => dispatch(fetchListings())
   });
->>>>>>> 1f0dbbb0e76963af6f1e0b73fb29429cd75b322c
 };
 
 const GoogleMapContainer =
