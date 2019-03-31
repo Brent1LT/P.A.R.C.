3f5416c351d1f46f6659df89ce83baee335fce4d
Sam Ardrey <samardrey@gmail.com>
2019-03-31 15:28:17 -0700

    Working on heroku

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const app = express();
 
 // This logic is used for Heroku deployment
 // to make sure we don't have "run build" before pushing to Heroku
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('frontend/build'));
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
 // }
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static('frontend/build'));
