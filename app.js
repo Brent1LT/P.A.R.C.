1c0227e18517bfad8e56c256f7a8daf161513361
Sam Ardrey <samardrey@gmail.com>
2019-04-04 14:18:44 -0700

    Work on map

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ const app = express();
 
 // This logic is used for Heroku deployment
 // to make sure we don't have "run build" before pushing to Heroku
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   });
-}
+// }
 
 mongoose
 .connect(db, {useNewUrlParser: true})
