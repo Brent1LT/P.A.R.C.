14b75166baa16d6e93f5f379078cd1e6b819e248
Vishavpreet Sandhu <vishal1949@gmail.com>
2019-04-05 11:06:19 -0700

    Add styling to listing index page

--- frontend/src/components/listing/listing_index_item.jsx
+++ frontend/src/components/listing/listing_index_item.jsx
@@ -7,11 +7,11 @@ class ListingIndexItem extends React.Component{
         <div id={this.props.listing._id} className="listing-item" >
           <img className='parking-logo' src={this.props.listing.photo} alt={this.props.listing.description} />
           <div className="listing-info">
-            <h3>Location: </h3>
             <div className='location-info'>{this.props.listing.street}</div>
             <div className='location-info'>{this.props.listing.city}, {this.props.listing.state}</div>
             <div className='graybar'></div>
-            <div className='description-listing'>Description: {this.props.listing.description}</div>
+            <div className='info-headings'>Description: <p>{this.props.listing.description}</p> </div>
+            {/* <div className='description-listing'>Description: {this.props.listing.description}</div> */}
             <div className='graybar'></div>
             <Link id='go-to-parking' to={`/listings/${this.props.listing._id}`}>Go to this parking!</Link>
           </div>
