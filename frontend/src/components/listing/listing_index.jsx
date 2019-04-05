9585a0b029815d57afdffe04c72dc6e6809819d4
Sam Ardrey <samardrey@gmail.com>
2019-04-04 20:11:42 -0700

    Fix listings search to show either nearest listings or all listings if none are near

--- frontend/src/components/listing/listing_index.jsx
+++ frontend/src/components/listing/listing_index.jsx
@@ -70,10 +70,22 @@ class ListingIndex extends React.Component{
       return null;
     }
 
-    const listingsArray = Object.values(this.props.listings);
     if(this.state.listing === null){
       return null;
     }
+
+    const listingsArray = Object.values(this.props.listings); //for mapping purposes
+    let listings; //for display purposes
+    if(this.state.lat !== 37.7599043 || this.state.lng !== -122.4256016){
+      listings = [];
+      listingsArray.map(listing => {
+        if ((listing.lat <= this.state.lat + .0055 && listing.lat >= this.state.lat - .0055) &&
+          (listing.lng <= this.state.lng + .0083 && listing.lng >= this.state.lng - .0083)) {
+          listings.push(listing);
+        }
+      });
+    }
+    if (!listings || listings.length === 0) listings = listingsArray;
 
     return (
       <div>
@@ -83,7 +95,7 @@ class ListingIndex extends React.Component{
         <div className='index-main-container'>
           <div id='goHere' className="all-listings">
             <div>
-              {listingsArray.map(listing => {
+              {listings.map(listing => {
                 return <ListingIndexItem listing={listing} />
               })}
             </div>
@@ -95,7 +107,7 @@ class ListingIndex extends React.Component{
                   lat={this.state.lat}
                   lng={this.state.lng}
                   changeListing={this.changeListing}
-                  listings={listingsArray}
+                  listings={listings}
                 />
               </div>
             </div>
